9fd85d20cdeb136cb9fbfbe90f6e1fea4a307571
Tim Hopper <tdhopper@gmail.com>
2016-10-20 14:27:08 -0400

    Fix syntax error

--- pywren/wren.py
+++ pywren/wren.py
@@ -3,7 +3,7 @@ import botocore
 import cloudpickle
 import json
 import base64
-from six.moves from six.moves import cPickle as pickle
+from six.moves import cPickle as pickle
 import wrenconfig
 import wrenutil
 import enum
