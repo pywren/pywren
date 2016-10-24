f2169d2e04178f1e01f5f730a4da22e344271707
Eric jonas <jonas@ericjonas.com>
2016-10-24 08:28:20 -0600

    updated s3 benchmark to stream

--- examples/s3_benchmark.py
+++ examples/s3_benchmark.py
@@ -1,7 +1,17 @@
-#from gevent import monkey
+"""
+Benchmark aggregate throughput from S3
 
-#monkey.patch_socket()
-#monkey.patch_ssl()
+Downloads a big file (~400 MB) from S3 several times
+per job, computes the md5sum, and returns the
+bandwidth time. 
+
+use:
+
+python s3_benchmark.py 100
+
+where 100 is the number of simultaneous workers. 
+
+"""
 
 import time
 import boto3 
@@ -13,22 +23,40 @@ import subprocess
 import logging
 import sys
 import boto3
+import hashlib
+import cPickle as pickle
+
+loops = 5
+blocksize = 1024*1024
 
 def run_command(x):
-    client = boto3.client('s3')
+    client = boto3.client('s3', 'us-west-2')
+
+    m = hashlib.md5()
+    bytes_read = 0
+
+    t1 = time.time()
+    for i in range(loops):
+        a = client.get_object(Bucket='ericmjonas-public',Key='anaconda.sh')
     
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('ericmjonas-public')
-    all_keys = []
-    for obj in bucket.objects.all():
-        all_keys.append(str(obj.key))
-    return all_keys
+        fileobj = a['Body']
+
+        buf = fileobj.read(blocksize)
+        while len(buf) > 0:
+            bytes_read += len(buf)
+            m.update(buf)
+            buf = fileobj.read(blocksize)
+    t2 = time.time()
+
+    a = m.hexdigest()
+    mb_rate = bytes_read/(t2-t1)/1e6
+    return t1, t2, mb_rate
 
 if __name__ == "__main__":
 
     wrenexec = pywren.default_executor()
-    fut = wrenexec.call_async(run_command, None)
-    print fut.callset_id
+    N = int(sys.argv[1])
+    fut = wrenexec.map(run_command, range(N))
 
-    res = fut.result() 
-    print res
+    res = [f.result() for f in fut]
+    pickle.dump(res, open('s3_benchmark.output.pickle', 'w'))
