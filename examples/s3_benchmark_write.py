e8f2d4c97913ffe4453809a4412e9c4c229d4940
Eric jonas <jonas@ericjonas.com>
2016-10-24 20:24:16 -0600

    streaming s3 benchmark

--- examples/s3_benchmark_write.py
+++ examples/s3_benchmark_write.py
@@ -24,34 +24,85 @@ import boto3
 import hashlib
 import cPickle as pickle
 import uuid
+import click
 
-if __name__ == "__main__":
-    bucket_name = sys.argv[1]
-    MB_to_write = int(sys.argv[2]) 
-    number_to_write = int(sys.argv[3])
+
+class RandomDataGenerator(object):
+
+    def __init__(self, bytes_total):
+        self.bytes_total = bytes_total
+        self.pos = 0
+
+    def tell(self):
+        print "tell", self.pos
+        return self.pos
+
+    def seek(self, pos, whence=0):
+        print "seek","pos=", pos, "whence=", whence
+        if whence == 0:
+            self.pos = pos
+        elif whence == 1:
+            self.pos += pos
+        elif whence == 2:
+            self.pos = self.bytes_total - pos
+
+    def read(self, bytes_requested):
+        remaining_bytes = self.bytes_total - self.pos
+        if remaining_bytes == 0:
+            print "requested", bytes_requested, "returning 0"
+            return ""
+        
+        data = np.arange(self.pos, self.pos + bytes_requested, dtype=np.uint8).tostring()
+        
+        bytes_out = min(remaining_bytes, bytes_requested)
+
+        byte_data = data[:bytes_out]
+                
+        self.pos += bytes_out
+
+        print "requested", bytes_requested, "returning", len(byte_data)
+
+        return byte_data
+
+@click.command()
+@click.option('--bucket_name', help='bucket to save files in')
+@click.option('--mb_per_file', help='MB of each object in S3', type=int)
+@click.option('--number', help='number of files', type=int)
+@click.option('--key_prefix', default='', help='S3 key prefix')
+@click.option('--outfile', default='s3_benchmark_write.output.pickle', 
+              help='filename to save results in')
+def benchmark(bucket_name, mb_per_file, number, key_prefix, outfile):
+
+    print "bucket_name =", bucket_name
+    print "mb_per_file =", mb_per_file
+    print "number=", number
+    print "key_prefix=", key_prefix
 
     def run_command(key_name):
-
-        data = np.random.rand(int(MB_to_write*1e6/8)).tostring()
-        actual_bytes = len(data)
+        bytes_n = mb_per_file * 1024**2
+        d = RandomDataGenerator(bytes_n)
 
         client = boto3.client('s3', 'us-west-2')
         t1 = time.time()
         client.put_object(Bucket=bucket_name, 
                           Key = key_name,
-                          Body=actual_bytes)
+                          Body=d)
         t2 = time.time()
 
 
-        mb_rate = actual_bytes/(t2-t1)/1e6
+        mb_rate = bytes_n/(t2-t1)/1e6
         return t1, t2, mb_rate
 
     wrenexec = pywren.default_executor()
 
     # create list of random keys
-    keynames = [ str(uuid.uuid4().get_hex().upper()) for _ in range(number_to_write)]
+    keynames = [ key_prefix + str(uuid.uuid4().get_hex().upper()) for _ in range(number)]
     
-    fut = wrenexec.map(run_command, range(N))
+    fut = wrenexec.map(run_command, keynames)
 
     res = [f.result() for f in fut]
-    pickle.dump(res, open('s3_benchmark_write.output.pickle', 'w'))
+    pickle.dump(res, open(outfile, 'w'))
+
+
+if __name__ == '__main__':
+    benchmark()
