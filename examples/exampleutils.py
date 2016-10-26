fe0c60c78905cf1aa9869f9430f00880236de5a7
Eric jonas <jonas@ericjonas.com>
2016-10-26 15:18:38 -0700

    example utils

---
+++ examples/exampleutils.py
@@ -0,0 +1,95 @@
+import numpy as np
+import time
+
+class RandomDataGenerator(object):
+    """
+    A file-like object which generates random data.
+    1. Never actually keeps all the data in memory so
+    can be used to generate huge files. 
+    2. Actually generates random data to eliminate
+    false metrics based on compression. 
+
+    It does this by generating data in 1MB blocks
+    from np.random where each block is seeded with
+    the block number. 
+    """
+
+    def __init__(self, bytes_total):
+        self.bytes_total = bytes_total
+        self.pos = 0
+        self.current_block_id = None
+        self.current_block_data = ""
+        self.BLOCK_SIZE_BYTES = 1024*1024
+    
+    def tell(self):
+        print "tell", self.pos
+        return self.pos
+
+    def seek(self, pos, whence=0):
+        print "seek","pos=", pos, "whence=", whence
+        if whence == 0:
+            self.pos = pos
+        elif whence == 1:
+            self.pos += pos
+        elif whence == 2:
+            self.pos = self.bytes_total - pos
+
+    def get_block(self, block_id):
+        if block_id == self.current_block_id:
+            return self.current_block_data
+
+        np.random.seed(block_id)
+        self.current_block_id = block_id
+        self.current_block_data = np.random.bytes(self.BLOCK_SIZE_BYTES)
+        return self.current_block_data
+    
+    def get_block_coords(self, abs_pos):
+        block_id = abs_pos // self.BLOCK_SIZE_BYTES
+        within_block_pos = abs_pos - block_id * self.BLOCK_SIZE_BYTES
+        return block_id, within_block_pos
+
+    def read(self, bytes_requested):
+        remaining_bytes = self.bytes_total - self.pos
+        if remaining_bytes == 0:
+            return ""
+        
+        bytes_out = min(remaining_bytes, bytes_requested)
+        start_pos = self.pos
+
+        byte_data = ""
+        byte_pos = 0
+        while byte_pos < bytes_out:
+            abs_pos = start_pos + byte_pos
+            bytes_remaining = bytes_out - byte_pos
+            
+            block_id, within_block_pos = self.get_block_coords(abs_pos)
+            block = self.get_block(block_id)
+            # how many bytes can we copy? 
+            chunk = block[within_block_pos:within_block_pos + bytes_remaining]
+
+            
+            byte_data += chunk
+
+            byte_pos += len(chunk)
+
+        self.pos += bytes_out
+
+        return byte_data
+
+
+if __name__ == "__main__":
+    """
+    basic benchmark of data generation
+    """
+    rdg = RandomDataGenerator(1024*1024*1024)
+    read_size = 8192
+    read_count = 10000000
+    bytes_read = 0
+    t1 = time.time()
+    for r in range(read_count):
+        a = rdg.read(read_size)
+        bytes_read += len(a)
+    t2 = time.time()
+
+    print t2-t1
+    print bytes_read / (t2-t1)/1e6, "MB/sec"
--- examples/s3_benchmark.py
+++ examples/s3_benchmark.py
@@ -1,15 +1,17 @@
 """
-Benchmark aggregate throughput from S3
+Benchmark aggregate read/write performance to S3
 
-Downloads a big file (~400 MB) from S3 several times
-per job, computes the md5sum, and returns the
-bandwidth time. 
+$ python s3_benchmark.py read --bucket_name=jonas-pywren-benchmark   --number=800 --key_file=big_keys.txt
 
-use:
+$ python s3_benchmark.py write --bucket_name=jonas-pywren-benchmark   --mb_per_file=1000 --number=800 --key_file=big_keys.txt
 
-python s3_benchmark.py 100
+Saves the output in a pickled list of IO transfer times. 
 
-where 100 is the number of simultaneous workers. 
+
+Note that you want to write to the root
+of an s3 bucket (no prefix) to get maximum
+performance as s3 apparently shards
+based on the first six chars of the keyname. 
 
 """
 
@@ -25,38 +27,115 @@ import sys
 import boto3
 import hashlib
 import cPickle as pickle
+import uuid
+import click
+import exampleutils
 
-loops = 5
-blocksize = 1024*1024
+@click.group()
+def cli():
+    pass
 
-def run_command(x):
-    client = boto3.client('s3', 'us-west-2')
 
-    m = hashlib.md5()
-    bytes_read = 0
+@cli.command()
+@click.option('--bucket_name', help='bucket to save files in')
+@click.option('--mb_per_file', help='MB of each object in S3', type=int)
+@click.option('--number', help='number of files', type=int)
+@click.option('--key_prefix', default='', help='S3 key prefix')
+@click.option('--outfile', default='s3_benchmark.write.output.pickle', 
+              help='filename to save results in')
+@click.option('--key_file', default=None, help="filename to write keynames to (for later read)")
+@click.option('--region', default='us-west-2', help="AWS Region")
+def write(bucket_name, mb_per_file, number, key_prefix, 
+              outfile, key_file, region):
 
-    t1 = time.time()
-    for i in range(loops):
-        a = client.get_object(Bucket='ericmjonas-public',Key='anaconda.sh')
-    
-        fileobj = a['Body']
+    print "bucket_name =", bucket_name
+    print "mb_per_file =", mb_per_file
+    print "number=", number
+    print "key_prefix=", key_prefix
 
-        buf = fileobj.read(blocksize)
-        while len(buf) > 0:
-            bytes_read += len(buf)
-            m.update(buf)
-            buf = fileobj.read(blocksize)
-    t2 = time.time()
+    def run_command(key_name):
+        bytes_n = mb_per_file * 1024**2
+        d = exampleutils.RandomDataGenerator(bytes_n)
 
-    a = m.hexdigest()
-    mb_rate = bytes_read/(t2-t1)/1e6
-    return t1, t2, mb_rate
+        client = boto3.client('s3', region)
+        t1 = time.time()
+        client.put_object(Bucket=bucket_name, 
+                          Key = key_name,
+                          Body=d)
+        t2 = time.time()
 
-if __name__ == "__main__":
+
+        mb_rate = bytes_n/(t2-t1)/1e6
+        return t1, t2, mb_rate
 
     wrenexec = pywren.default_executor()
-    N = int(sys.argv[1])
-    fut = wrenexec.map(run_command, range(N))
+
+    # create list of random keys
+    keynames = [ key_prefix + str(uuid.uuid4().get_hex().upper()) for _ in range(number)]
+    
+    if key_file is not None:
+        fid = open(key_file, 'w')
+        for k in keynames:
+            fid.write("{}\n".format(k))
+                
+    fut = wrenexec.map(run_command, keynames)
 
     res = [f.result() for f in fut]
-    pickle.dump(res, open('s3_benchmark_read.output.pickle', 'w'))
+    pickle.dump(res, open(outfile, 'w'))
+
+@cli.command()
+@click.option('--bucket_name', help='bucket to save files in')
+@click.option('--number', help='number of objects to read', type=int)
+@click.option('--outfile', default='s3_benchmark_read.output.pickle', 
+              help='filename to save results in')
+@click.option('--key_file', default=None, help="filename to write keynames to (for later read)")
+@click.option('--s3_key', default=None, help="s3 key to read (repeat)")
+@click.option('--read_times', default=1, help="number of times to read each s3 key")
+@click.option('--region', default='us-west-2', help="AWS Region")
+def read(bucket_name, number, 
+         outfile, key_file, s3_key, read_times, region):
+    if key_file is None and s3_key is None:
+        print "must specify either a single key to repeatedly read ( --s3_key) or a text file with keynames (--key_file)"
+        sys.exit(1)
+    
+    blocksize = 1024*1024
+
+    def run_command(key):
+        client = boto3.client('s3', region)
+
+        m = hashlib.md5()
+        bytes_read = 0
+
+        t1 = time.time()
+        for i in range(read_times):
+            obj = client.get_object(Bucket=bucket_name, Key=key)
+
+            fileobj = obj['Body']
+
+            buf = fileobj.read(blocksize)
+            while len(buf) > 0:
+                bytes_read += len(buf)
+                m.update(buf)
+                buf = fileobj.read(blocksize)
+        t2 = time.time()
+
+        a = m.hexdigest()
+        mb_rate = bytes_read/(t2-t1)/1e6
+        return t1, t2, mb_rate, 
+
+    wrenexec = pywren.default_executor()
+    
+    if s3_key is not None:
+        keylist = [s3_key] * number
+    else:
+        fid = open(key_file, 'r')
+        keylist_raw = [k.strip() for k in fid.readlines()]
+        keylist = [keylist_raw[i % len(keylist_raw)]  for i in range(number)]
+
+    fut = wrenexec.map(run_command, keylist)
+
+    res = [f.result() for f in fut]
+    pickle.dump(res, open('s3_benchmark.read.output.pickle', 'w'))
+
+if __name__ == '__main__':
+    cli()
