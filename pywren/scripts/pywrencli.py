bb8853c546ceb39cef79d096d5d05efca8104282
Eric jonas <jonas@ericjonas.com>
2017-01-18 21:25:02 -0800

    string conversion

--- pywren/scripts/pywrencli.py
+++ pywren/scripts/pywrencli.py
@@ -88,7 +88,7 @@ def create_role():
     
     AWS_ACCOUNT_ID = config['account']['aws_account_id']
     AWS_REGION = config['account']['aws_region']
-    more_json_policy = more_json_policy.replace("AWS_ACCOUNT_ID", AWS_ACCOUNT_ID)
+    more_json_policy = more_json_policy.replace("AWS_ACCOUNT_ID", str(AWS_ACCOUNT_ID))
     more_json_policy = more_json_policy.replace("AWS_REGION", AWS_REGION)
 
     iamclient.RolePolicy(role_name, '{}-more-permissions'.format(role_name)).put(
