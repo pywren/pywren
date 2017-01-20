b243eff2f94b617242f054d72951a871e51ded77
Eric jonas <jonas@ericjonas.com>
2017-01-19 16:40:17 -0800

    install python redis bindings

--- fabfile_builder.py
+++ fabfile_builder.py
@@ -110,6 +110,7 @@ def conda_setup_mkl_avx2():
             #run("conda clean -y -i -t -p")
             run("pip install --upgrade cloudpickle")
             run("pip install cvxpy")
+            run("pip install redis")
 
 @task
 def conda_setup_nomkl():
