1b62fb7a194cd73dd031d6e681cb4f63df4b2196
Eric jonas <jonas@ericjonas.com>
2017-02-01 11:22:49 -0800

    clean up tests

--- tests/test_dummy_invoker.py
+++ tests/test_dummy_invoker.py
@@ -11,44 +11,44 @@ import unittest
 import numpy as np
 from flaky import flaky
 
-# class SimpleAsync(unittest.TestCase):
+class SimpleAsync(unittest.TestCase):
 
-#     def setUp(self):
-#         self.wrenexec = pywren.dummy_executor()
+    def setUp(self):
+        self.wrenexec = pywren.dummy_executor()
 
-#     def test_simple(self):
+    def test_simple(self):
 
-#         def sum_list(x):
-#             return np.sum(x)
+        def sum_list(x):
+            return np.sum(x)
 
-#         x = np.arange(10)
-#         fut = self.wrenexec.call_async(sum_list, x)
+        x = np.arange(10)
+        fut = self.wrenexec.call_async(sum_list, x)
         
-#         self.wrenexec.invoker.run_jobs()
+        self.wrenexec.invoker.run_jobs()
 
-#         res = fut.result() 
-#         self.assertEqual(res, np.sum(x))
+        res = fut.result() 
+        self.assertEqual(res, np.sum(x))
 
-#     def test_exception(self):
-#         def throwexcept(x):
-#             raise Exception("Throw me out!")
+    def test_exception(self):
+        def throwexcept(x):
+            raise Exception("Throw me out!")
 
-#         wrenexec = pywren.default_executor()
-#         fut = self.wrenexec.call_async(throwexcept, None)
-#         self.wrenexec.invoker.run_jobs()
+        wrenexec = pywren.default_executor()
+        fut = self.wrenexec.call_async(throwexcept, None)
+        self.wrenexec.invoker.run_jobs()
         
-#         with pytest.raises(Exception) as execinfo:
-#             res = fut.result() 
-#         assert 'Throw me out!' in str(execinfo.value)
+        with pytest.raises(Exception) as execinfo:
+            res = fut.result() 
+        assert 'Throw me out!' in str(execinfo.value)
 
 
-#     def test_subprocess(self):
-#         def uname(x):
-#             return subprocess.check_output("uname -a", shell=True)
+    def test_subprocess(self):
+        def uname(x):
+            return subprocess.check_output("uname -a", shell=True)
         
-#         fut = self.wrenexec.call_async(uname, None)
-#         self.wrenexec.invoker.run_jobs()
+        fut = self.wrenexec.call_async(uname, None)
+        self.wrenexec.invoker.run_jobs()
 
-#         res = fut.result() 
+        res = fut.result() 
 
 
