84631e3214ae907f6bef2747359d70e50ff81f12
Eric jonas <jonas@ericjonas.com>
2017-02-05 15:49:52 -0800

    added extra test

--- tests/ec2_standalone_tests.sh
+++ tests/ec2_standalone_tests.sh
@@ -2,5 +2,5 @@
 set -e
 pywren standalone launch_instances 1 --max_idle_time=10 --idle_terminate_granularity=600
 sleep 10
-PYWREN_EXECUTOR=lambda pytest tests/test_simple.py
+PYWREN_EXECUTOR=lambda pytest tests/test_simple.py tests/test_modules.py
 pywren standalone terminate_instances
