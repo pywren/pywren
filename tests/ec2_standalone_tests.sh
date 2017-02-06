918473d9754a987298046e76f42bfac0476b1d11
Eric jonas <jonas@ericjonas.com>
2017-02-05 18:22:37 -0800

    only run these stand-alone tests

--- tests/ec2_standalone_tests.sh
+++ tests/ec2_standalone_tests.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 pywren standalone launch_instances 1 --max_idle_time=10 --idle_terminate_granularity=600
-sleep 10
-PYWREN_EXECUTOR=remote pytest tests/test_simple.py tests/test_modules.py
+sleep 20
+PYWREN_EXECUTOR=remote pytest tests/test_simple.py 
 pywren standalone terminate_instances
