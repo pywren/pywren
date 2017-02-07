11fe452a6d48c46b5c27d28b7231c55c2f954ce4
Eric jonas <jonas@ericjonas.com>
2017-02-07 07:14:22 -0800

    verbose tests

--- tests/ec2_standalone_tests.sh
+++ tests/ec2_standalone_tests.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 pywren standalone launch_instances 1 --max_idle_time=10 --idle_terminate_granularity=600
 sleep 20
-PYWREN_EXECUTOR=remote pytest tests/test_simple.py
+PYWREN_EXECUTOR=remote pytest -v tests/test_simple.py
 pywren standalone terminate_instances
