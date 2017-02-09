5f5f99e90132b412542fa9b01a1dd81d0514cc4b
Eric jonas <jonas@ericjonas.com>
2017-02-09 16:51:39 -0600

    added macroreduce example

---
+++ examples/macroreduce.py
@@ -0,0 +1,19 @@
+import pywren
+
+if __name__ == "__main__":
+
+    lambda_exec = pywren.lambda_executor()
+    remote_exec = pywren.remote_executor()
+
+
+    def increment(x):
+        return x+1
+
+    x = [1, 2, 3, 4]
+    futures = lambda_exec.map(increment, x)
+
+    def reduce_func(x):
+        return sum(x)
+
+    reduce_future = lambda_exec.reduce(reduce_func, futures)
+    print reduce_future.result()
