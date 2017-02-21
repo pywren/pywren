a5da52fad2aa69e61a7452d69ffc9146c74c2d8c
Eric Jonas <jonas@ericjonas.com>
2017-02-21 13:25:42 -0800

    how many different ways do we specify files?

--- pywren/version.py
+++ pywren/version.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 # we're following the version number guidelines
 # from https://www.python.org/dev/peps/pep-0386/
 
-__version__ = "0.0.4rc1"
+__version__ = "0.0.4rc2"
 
 if __name__ == "__main__":
     print(__version__)
