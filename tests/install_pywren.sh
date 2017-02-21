3778c3887e364e09a76108f38ef7163b3cacc699
Eric Jonas <jonas@ericjonas.com>
2017-02-21 12:16:52 -0800

    added echo for install; updated contributors

--- tests/install_pywren.sh
+++ tests/install_pywren.sh
@@ -4,10 +4,12 @@ set -ev
 # this is a script that either installs the local pywren from source
 # or downloads from pypy depending on the branch name
 if [ ${TRAVIS_TAG} = "pypitest-build" ]; then
-    rm -Rf pywren
-    pip install --extra-index-url https://testpypi.python.org/pypi pywren
+    echo "installing from pypitest"; 
+    rm -Rf pywren; 
+    pip install --extra-index-url https://testpypi.python.org/pypi pywren; 
 else
-    python setup.py install
+    echo "installing from git"; 
+    python setup.py install; 
 fi
 
 exit 0;
