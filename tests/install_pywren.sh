e72f2f138757e61819d7ec27cd80ddaf05bbb9af
Eric Jonas <jonas@ericjonas.com>
2017-02-21 09:53:34 -0800

    include script

---
+++ tests/install_pywren.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+# this is a script that either installs the local pywren from source
+# or downloads from pypy depending on the branch name
+if [ ${TRAVIS_BRANCH} = "pypitest-build" ]; then
+    rm -Rf pywren
+    pip install -i https://testpypi.python.org/pypi pywren
+else
+    python setup.py install
+fi
+
+exit 0;
