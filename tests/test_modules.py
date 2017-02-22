d518cf27a7754453c4c2c9666b1cfa3441ef121e
Eric Jonas <jonas@ericjonas.com>
2017-02-22 11:06:58 -0800

    modules fail, yay

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -28,7 +28,29 @@ class SimpleAsync(unittest.TestCase):
         self.assertEqual(res, 2.0)
 
     def test_utf8_module(self):
-        pass
+        def foo(x):
+            return extmoduleutf8.foo_add(x)
+
+        x = 1.0
+        fut = self.wrenexec.call_async(foo, x)
+        
+        self.wrenexec.invoker.run_jobs()
+
+        res = fut.result() 
+        self.assertEqual(res, 2.0)
+
+    def test_utf8_string(self):
+        def foo(x):
+            return extmoduleutf8.unicode_str(x)
+
+        x = 1.0
+        fut = self.wrenexec.call_async(foo, x)
+        
+        self.wrenexec.invoker.run_jobs()
+
+        res = fut.result() 
+        self.assertEqual(res, extmoduleutf8.TEST_STR)
+
 
 class DummyExecutorImport(unittest.TestCase):
 
@@ -39,7 +61,6 @@ class DummyExecutorImport(unittest.TestCase):
 
         def foo(x):
             return extmoduleutf8.foo_add(x)
-
 
         x = 1.0
         fut = self.wrenexec.call_async(foo, x)
