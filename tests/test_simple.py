ede28144c242c46814bdc8ee6a2283e5a99675b7
Qifan Pu <qifan.pu@gmail.com>
2017-02-26 14:27:59 -0800

    Support wait ANY_COMPLETED

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -223,5 +223,44 @@ class ConfigErrors(unittest.TestCase):
                 with pytest.raises(Exception) as excinfo:
                     pywren.lambda_executor(config)
                 assert 'python version' in str(excinfo.value)
-                        
 
+# skipping those tests in CI because of stability concerns
+@pytest.mark.skip                    
+class WaitTest(unittest.TestCase):
+    # FIXME: for stability, we should proabbly use the local dummy executor,
+    # but currently the dummy invoker impl does not support wait()
+    def setUp(self):
+        self.wrenexec = pywren.default_executor()
+
+    def test_all_complete(self):
+        def wait_10x_sec_and_plus_one(x):
+            time.sleep(10*x)
+            return x + 1
+
+        N = 3
+        x = np.arange(N)
+
+        futures = pywren.default_executor().map(wait_10x_sec_and_plus_one, x)
+
+        fs_dones, fs_notdones = pywren.wait(futures,
+                                        return_when=pywren.wren.ALL_COMPLETED)
+        res =np.array([f.result() for f in fs_dones])
+        np.testing.assert_array_equal(res, x+1)
+
+    def test_any_complete(self):
+        def wait_10x_sec_and_plus_one(x):
+            time.sleep(10*x)
+            return x + 1
+
+        N = 3
+        x = np.arange(N)
+
+        futures = pywren.default_executor().map(wait_10x_sec_and_plus_one, x)
+
+        fs_notdones = futures
+        for xi in x:
+            fs_dones, fs_notdones = pywren.wait(fs_notdones,
+                                            return_when=pywren.wren.ANY_COMPLETED,
+                                            WAIT_DUR_SEC=1)
+            res =np.array([f.result() for f in fs_dones])
+            np.testing.assert_array_equal(res, [xi+1])
