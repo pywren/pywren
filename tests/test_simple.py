b2e15e0d8ebf0b6f42d3ef22789a36b72be6eb32
Qifan Pu <qifan.pu@gmail.com>
2017-02-26 22:46:55 -0800

    Fix test not to rely on orders.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -225,41 +225,39 @@ class ConfigErrors(unittest.TestCase):
                 assert 'python version' in str(excinfo.value)
 
 class WaitTest(unittest.TestCase):
-    # FIXME: for stability, we should proabbly use the local dummy executor,
-    # but currently the dummy invoker impl does not support wait()
     def setUp(self):
         self.wrenexec = pywren.default_executor()
 
     def test_all_complete(self):
-        def wait_10x_sec_and_plus_one(x):
-            time.sleep(10*x)
+        def wait_x_sec_and_plus_one(x):
+            time.sleep(x)
             return x + 1
 
-        N = 3
+        N = 10
         x = np.arange(N)
 
-        futures = pywren.default_executor().map(wait_10x_sec_and_plus_one, x)
+        futures = pywren.default_executor().map(wait_x_sec_and_plus_one, x)
 
         fs_dones, fs_notdones = pywren.wait(futures,
                                         return_when=pywren.wren.ALL_COMPLETED)
-        res =np.array([f.result() for f in fs_dones])
+        res = np.array([f.result() for f in fs_dones])
         np.testing.assert_array_equal(res, x+1)
 
-    @flaky(max_runs=3)
     def test_any_complete(self):
-        def wait_10x_sec_and_plus_one(x):
-            time.sleep(10*x)
+        def wait_x_sec_and_plus_one(x):
+            time.sleep(x)
             return x + 1
 
-        N = 3
+        N = 10
         x = np.arange(N)
 
-        futures = pywren.default_executor().map(wait_10x_sec_and_plus_one, x)
+        futures = pywren.default_executor().map(wait_x_sec_and_plus_one, x)
 
         fs_notdones = futures
-        for xi in x:
+        while (len(fs_notdones) > 0):
             fs_dones, fs_notdones = pywren.wait(fs_notdones,
                                             return_when=pywren.wren.ANY_COMPLETED,
                                             WAIT_DUR_SEC=1)
-            res =np.array([f.result() for f in fs_dones])
-            np.testing.assert_array_equal(res, [xi+1])
+            self.assertTrue(len(fs_dones) > 0)
+        res = np.array([f.result() for f in futures])
+        np.testing.assert_array_equal(res, x+1)
