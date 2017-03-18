f44dbfdd60edbca6c61ca50fe7fd72589a90bf2e
Eric Jonas <jonas@ericjonas.com>
2017-03-18 06:45:20 -0700

    bump version

--- pywren/version.py
+++ pywren/version.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 # we're following the version number guidelines
 # from https://www.python.org/dev/peps/pep-0386/
 
-__version__ = "0.1"
+__version__ = "0.2dev"
 
 if __name__ == "__main__":
     print(__version__)
