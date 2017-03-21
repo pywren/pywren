4b90869fc1d7381a8587e49ce2ea112e11d6152d
Eric Jonas <jonas@ericjonas.com>
2017-03-21 06:37:08 -0700

    added numba test

---
+++ tests/test_numba.py
@@ -0,0 +1,60 @@
+import pywren
+from numba import jit
+import time
+
+import unittest
+
+def foo(N):
+    x = 0.0
+    for i in range(N):
+        x += 1.0
+    return x
+
+@jit
+def bar(N):
+    x = 0.0
+    for i in range(N):
+        x += 1.0
+    return x
+
+
+# Time isn't supported in numba so we have to wrap
+def time_foo(N):
+    t1 = time.time()
+    for i in range(5):
+        foo(N)
+    t2 = time.time()
+    return t2-t1
+
+def time_bar(N):
+    t1 = time.time()
+    for i in range(5):
+        bar(N)
+    t2 = time.time()
+    return t2-t1
+
+
+class NumbaTest(unittest.TestCase):
+
+    def setUp(self):
+        self.wrenexec = pywren.default_executor()
+
+    def test_numba(self):
+        """
+        Simple numba test, compares two loops, makes sure
+        one runs much faster than the other
+
+        """
+        
+
+        N = 10000000
+        results = self.wrenexec.map(time_foo, [N])
+        pywren.wait(results)
+        regular_time = results[0].result()
+
+        results = self.wrenexec.map(time_bar, [N])
+        pywren.wait(results)
+        numba_time = results[0].result()
+        self.assertTrue(numba_time < regular_time / 10.0)
+
+
