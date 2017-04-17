3bb8eaa0b70e183aa0a6aeea8aa6bf6ca3320940
Qifan Pu <qifan.pu@gmail.com>
2017-04-17 10:04:42 -0700

    Save ssh key in create_ssh_key()

--- pywren/scripts/pywrencli.py
+++ pywren/scripts/pywrencli.py
@@ -152,7 +152,9 @@ def create_role(ctx):
 
 @click.command()
 @click.pass_context
-def create_ssh_key(ctx):
+@click.option('--key_file_save_location', default=None,
+              help="The directory which key file would be saved.")
+def create_ssh_key(ctx, key_file_save_location=None):
     """
 
     """
@@ -163,10 +165,18 @@ def create_ssh_key(ctx):
     ec2 = boto3.client('ec2', region)
     keyname = config['standalone']['ec2_ssh_key']
     try:
-        ec2.describe_key_pairs(KeyNames = [keyname])
+        ec2.describe_key_pairs(KeyNames=[keyname])
+        print("EC2 SSH key " + keyname + " exists already.")
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == 'InvalidKeyPair.NotFound':
-            ec2.create_key_pair(KeyName = keyname)
+            key = ec2.create_key_pair(KeyName=keyname)
+            print("EC2 SSH key " + keyname + " is created.")
+            if key_file_save_location is not None:
+                filename = os.path.join(key_file_save_location, keyname + ".pem")
+                with open(filename, "w") as f:
+                    f.write(key['KeyMaterial'])
+                f.close()
+                print("SSH private key " + keyname + " is saved to " + filename)
 
 
 @click.command()
