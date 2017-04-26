5e46cae83d2241375ebd9ed0b670e8b4f618bbcf
Qifan Pu <qifan.pu@gmail.com>
2017-04-26 11:28:18 -0700

    Initial change to executor.py

--- pywren/executor.py
+++ pywren/executor.py
@@ -6,8 +6,10 @@ try:
     from six.moves import cPickle as pickle
 except:
     import pickle
+import multiprocessing
 from multiprocessing.pool import ThreadPool
 import time
+import enum
 import random
 import logging
 import botocore
@@ -25,6 +27,163 @@ from pywren.future import ResponseFuture, JobState
 from pywren.wait import *
 
 logger = logging.getLogger(__name__)
+
+
+class SchedulerCommand(enum.Enum):
+    NO_MORE_JOB = 1
+
+def start_scheduler(q):
+
+    while True:
+        if not q.empty():
+            job = q.get()
+            if job == SchedulerCommand.NO_MORE_JOB:
+                return
+            print(job)
+
+
+class NewExecutor(object):
+
+    def __init__(self, invoker, config, job_max_runtime):
+        self.invoker = invoker
+        self.job_max_runtime = job_max_runtime
+
+        self.config = config
+        self.storage_config = wrenconfig.extract_storage_config(self.config)
+        self.storage = storage.Storage(self.storage_config)
+        self.runtime_meta_info = runtime.get_runtime_info(config['runtime'], self.storage)
+
+
+        if 'preinstalls' in self.runtime_meta_info:
+            logger.info("using serializer with meta-supplied preinstalls")
+            self.serializer = serialize.SerializeIndependent(self.runtime_meta_info['preinstalls'])
+        else:
+            self.serializer =  serialize.SerializeIndependent()
+
+        self.job_queue = multiprocessing.Queue()
+
+
+    def create_scheduler(self):
+        self.scheduler = None
+
+    def map(self, func, iterdata, extra_env = None, extra_meta = None,
+            invoke_pool_threads=64, data_all_as_one=True,
+            use_cached_runtime=True, overwrite_invoke_args = None):
+        """
+
+
+
+        data_all_as_one : upload the data as a single object; fewer
+        tcp transactions (good) but potentially higher latency for workers (bad)
+
+        use_cached_runtime : if runtime has been cached, use that. When set
+        to False, redownloads runtime.
+        """
+
+        host_job_meta = {}
+
+        pool = ThreadPool(invoke_pool_threads)
+        callset_id = wrenutil.create_callset_id()
+        data = list(iterdata)
+
+        ### pickle func and all data (to capture module dependencies
+        func_and_data_ser, mod_paths = self.serializer([func] + data)
+
+        func_str = func_and_data_ser[0]
+        data_strs = func_and_data_ser[1:]
+        data_size_bytes = sum(len(x) for x in data_strs)
+        agg_data_key = None
+        host_job_meta['agg_data'] = False
+        host_job_meta['data_size_bytes'] =  data_size_bytes
+
+        if data_size_bytes < wrenconfig.MAX_AGG_DATA_SIZE and data_all_as_one:
+            agg_data_key = self.storage.create_agg_data_key(callset_id)
+            agg_data_bytes, agg_data_ranges = self.agg_data(data_strs)
+            agg_upload_time = time.time()
+            self.storage.put_object(agg_data_key, agg_data_bytes)
+            host_job_meta['agg_data'] = True
+            host_job_meta['data_upload_time'] = time.time() - agg_upload_time
+            host_job_meta['data_upload_timestamp'] = time.time()
+        else:
+            # FIXME add warning that you wanted data all as one but
+            # it exceeded max data size
+            pass
+
+
+        module_data = create_mod_data(mod_paths)
+        func_str_encoded = wrenutil.bytes_to_b64str(func_str)
+        #debug_foo = {'func' : func_str_encoded,
+        #             'module_data' : module_data}
+
+        #pickle.dump(debug_foo, open("/tmp/py35.debug.pickle", 'wb'))
+        ### Create func and upload
+        func_module_str = json.dumps({'func' : func_str_encoded,
+                                      'module_data' : module_data})
+        host_job_meta['func_module_str_len'] = len(func_module_str)
+
+        func_upload_time = time.time()
+        func_key = self.storage.create_func_key(callset_id)
+        self.storage.put_object(func_key, func_module_str)
+        host_job_meta['func_upload_time'] = time.time() - func_upload_time
+        host_job_meta['func_upload_timestamp'] = time.time()
+        def invoke(data_str, callset_id, call_id, func_key,
+                   host_job_meta,
+                   agg_data_key = None, data_byte_range=None ):
+            data_key, output_key, status_key \
+                = self.storage.create_keys(callset_id, call_id)
+
+            host_job_meta['job_invoke_timestamp'] = time.time()
+
+            if agg_data_key is None:
+                data_upload_time = time.time()
+                self.put_data(data_key, data_str,
+                              callset_id, call_id)
+                data_upload_time = time.time() - data_upload_time
+                host_job_meta['data_upload_time'] = data_upload_time
+                host_job_meta['data_upload_timestamp'] = time.time()
+
+                data_key = data_key
+            else:
+                data_key = agg_data_key
+
+            return self.invoke_with_keys(func_key, data_key,
+                                         output_key,
+                                         status_key,
+                                         callset_id, call_id, extra_env,
+                                         extra_meta, data_byte_range,
+                                         use_cached_runtime, host_job_meta.copy(),
+                                         self.job_max_runtime,
+                                         overwrite_invoke_args = overwrite_invoke_args)
+
+        N = len(data)
+        call_result_objs = []
+        for i in range(N):
+            call_id = "{:05d}".format(i)
+
+            data_byte_range = None
+            if agg_data_key is not None:
+                data_byte_range = agg_data_ranges[i]
+
+            cb = pool.apply_async(invoke, (data_strs[i], callset_id,
+                                           call_id, func_key,
+                                           host_job_meta.copy(),
+                                           agg_data_key,
+                                           data_byte_range))
+
+            logger.info("map {} {} apply async".format(callset_id, call_id))
+
+            call_result_objs.append(cb)
+
+        res =  [c.get() for c in call_result_objs]
+        pool.close()
+        pool.join()
+        logger.info("map invoked {} {} pool join".format(callset_id, call_id))
+
+        # FIXME take advantage of the callset to return a lot of these
+
+        # note these are just the invocation futures
+
+        return res
 
 
 class Executor(object):
