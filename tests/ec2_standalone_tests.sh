8f562016a7b034a5bcf60342354e2990316d2ec0
Eric Jonas <jonas@ericjonas.com>
2017-09-11 10:50:43 -0700

    added bash script to wait for instance profile to propagate in tests

--- tests/ec2_standalone_tests.sh
+++ tests/ec2_standalone_tests.sh
@@ -4,7 +4,18 @@ set -x
 if [ "$RUN_STANDALONE" != "true" ]; then
     exit 0
 fi
+# sometimes the instance profile isn't visible yet to the function.
+# this results in erratic test behavior and tests sometimes failing
 
+n=0
+until [ $n -ge 5 ]
+do
+    aws get-instance-profile --instance-profile-name pywren_travis_$BUILD_GUID && break  # substitute your command here
+    echo "instance profile was not available, retrying"
+    n=$[$n+1]
+    sleep 10
+done
+   
 pywren standalone launch_instances 1 --max_idle_time=10 --idle_terminate_granularity=600 --pywren_git_commit=$TRAVIS_COMMIT
 sleep 20
 export PYWREN_EXECUTOR=remote
