6fff11243b03c799ad883c947ced6a8cc570da3e
Vaishaal Shankar <vaishaal@berkeley.edu>
2017-09-15 05:46:13 +0000

    python3 support

--- pywren/wrenhandler.py
+++ pywren/wrenhandler.py
@@ -14,7 +14,12 @@ import tarfile
 import time
 import traceback
 from threading import Thread
-from urllib2 import urlopen
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
 
 import boto3
 import botocore
