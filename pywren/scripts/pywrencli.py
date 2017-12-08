67c972b045e805a284c1ad2be6d4e5f0dc7436db
Eric Jonas <jonas@ericjonas.com>
2017-12-07 19:27:43 -0800

    lambda deploy bug

--- pywren/scripts/pywrencli.py
+++ pywren/scripts/pywrencli.py
@@ -215,7 +215,7 @@ def deploy_lambda(ctx, update_if_exists=True):
     module_dir = os.path.join(SOURCE_DIR, "../")
 
     for f in ['wrenutil.py', 'wrenconfig.py', 'wrenhandler.py',
-              'version.py', 'jobrunner.py', 'wren.py']:
+              'version.py', 'jobrunner/jobrunner.py', 'wren.py']:
         f = os.path.abspath(os.path.join(module_dir, f))
         a = os.path.relpath(f, SOURCE_DIR + "/..")
 
