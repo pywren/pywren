5d7475edcb22c4b46615d19ae52ddb2b3d80062f
Vaishaal Shankar <vaishaal@berkeley.edu>
2018-02-21 22:31:36 +0000

    added test for many standalone executors

---
+++ tests/test_standalone_many.py
@@ -0,0 +1,37 @@
+import pytest
+import time
+import boto3 
+import uuid
+import numpy as np
+import time
+import os
+import pywren
+import pywren.runtime
+import subprocess
+import logging
+from six.moves import cPickle as pickle
+
+import unittest
+import numpy as np
+from flaky import flaky
+import sys
+
+
+
+class StandaloneMany(unittest.TestCase):
+
+    def setUp(self):
+        self.wrenexec = pywren.default_executor()
+
+    def test_parallel(self):
+        EXECUTOR_PARALLELISM = int(os.environ["EXECUTOR_PARALLELISM"])
+        N = EXECUTOR_PARALLELISM
+        def test_fn(i):
+            open("/tmp/potato_{0}".format(i), "w+").close()
+            time.sleep(120)
+            return [x for x in os.listdir("/tmp/") if "potato" in x]
+
+        futures = self.wrenexec.map(test_fn, range(EXECUTOR_PARALLELISM))
+        for f in futures:
+            assert(len(f.result()) == EXECUTOR_PARALLELISM)
+
